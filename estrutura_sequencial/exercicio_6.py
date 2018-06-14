c624bee2b8f2bd3f364cf7e1ee26109f304efe15
danielprata <daniel.prata@4linux.com.br>
2018-06-14 01:11:58 -0300

    solucao exercicios

--- estrutura_sequencial/exercicio_6.py
+++ estrutura_sequencial/exercicio_6.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 '''Faça um Programa que peça o raio de um círculo, 
 calcule e mostre sua área '''
+
+raio = float(input('Digite um raio de um circulo: '))
+area = raio ** 2
+print("A area do circulo é igual há {}".format(area))
--- estrutura_sequencial/exercicio_7.py
+++ estrutura_sequencial/exercicio_7.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 '''Faça um Programa que calcule a área de um quadrado, 
 em seguida mostre o dobro desta área para o usuário.'''
+
+lado = float(input('Digite o valor de um lado do quadrado: '))
+area = lado * lado
+area_dobro = area * 2
+print("O dobro da area do quadradro é igual há {}".format(area_dobro))
