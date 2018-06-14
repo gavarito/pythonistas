71e8552c27c0ff09f1b29000bf48e40f9e1bfdc2
danielprata <daniel.prata@4linux.com.br>
2018-06-14 00:31:23 -0300

    solucação exercicio_5

--- estrutura_sequencial/exercicio_5.py
+++ estrutura_sequencial/exercicio_5.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 '''Faça um Programa que converta metros para centímetros. '''
+
+metros = float(input('Digite o valor em metros que deseja converter: '))
+
+cm = metros * 100
+
+print('{}m é igual há {}cm'.format(metros, cm))
