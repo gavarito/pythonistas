609385e795a22086fc1b4d709f86d5f34a12daa2
pratadaniel94 <danielprata@outlook.com>
2018-06-14 21:34:34 -0300

    melhora exercicio6

--- estrutura_sequencial/exercicio_6.py
+++ estrutura_sequencial/exercicio_6.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from math import pi
 '''Faça um Programa que peça o raio de um círculo, 
 calcule e mostre sua área '''
 
 raio = float(input('Digite um raio de um circulo: '))
-area = raio ** 2
-print("A area do circulo é igual há {}".format(area))
+area = pi * (raio ** 2)
+print("A area do circulo é igual há {:.2f}".format(area))
