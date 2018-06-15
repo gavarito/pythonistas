259bc6bd192b80af57e2de2ccc39c113850e0d38
pratadaniel94 <danielprata@outlook.com>
2018-06-14 21:05:52 -0300

    melhoria exercicio4

--- estrutura_sequencial/exercicio_4.py
+++ estrutura_sequencial/exercicio_4.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 ''' Faça um Programa que peça as 4 notas bimestrais e mostre a média. '''
 
-nota1 = float(input('digite a nota 1: '))
-nota2 = float(input('digite a nota 2: '))
-nota3 = float(input('digite a nota 3: '))
-nota4 = float(input('digite a nota 4: '))
+# nota1 = float(input('digite a nota 1: '))
+# nota2 = float(input('digite a nota 2: '))
+# nota3 = float(input('digite a nota 3: '))
+# nota4 = float(input('digite a nota 4: '))
 
-media = (nota1 + nota2 + nota3 + nota4) / 4
+# media = (nota1 + nota2 + nota3 + nota4) / 4
 
-print('A média é igual há {}'.format(media))
+# print('A média é igual há {:.2f}'.format(media))
+
+soma = 0
+for x in range(4):
+    nota = float(input('digite a nota {}: '.format(x+1)))
+    soma += nota
+print('A média é igual há {:.2f}'.format(soma/4))
